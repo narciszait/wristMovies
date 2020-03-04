42f98fb04259d6fce2e34138b572c6ec128e451b
Narcis Zait <narcis.zait@gmail.com>
2020-03-04 19:20:38 +0100

    first commit

---
+++ Wrist Movies WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Wrist Movies WatchKit Extension
+//
+//  Created by Narcis Zait on 04/03/2020.
+//  Copyright © 2020 Narcis Zait. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
